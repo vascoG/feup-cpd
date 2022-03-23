0798a4ebdd60a52e9c7389886d19d9e971da8022
Vasco David Antunes Pereira Gomes <up201906617@fe.up.pt>
2022-03-23 10:52:54 +0000

    Update matrixproduct.cpp

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <iostream>
+#include <iomanip>
 #include <time.h>
+#include <cstdlib>
+#include <papi.h>
 
 
 using namespace std;
@@ -208,6 +211,25 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
 
 }
 
+void handle_error (int retval)
+{
+  printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
+  exit(1);
+}
+
+void init_papi() {
+  int retval = PAPI_library_init(PAPI_VER_CURRENT);
+  if (retval != PAPI_VER_CURRENT && retval < 0) {
+    printf("PAPI library version mismatch!\n");
+    exit(1);
+  }
+  if (retval < 0) handle_error(retval);
+
+  std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
+            << " MINOR: " << PAPI_VERSION_MINOR(retval)
+            << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
+}
+
 
 
 
@@ -217,6 +239,28 @@ int main (int argc, char *argv[])
 	char c;
 	int lin, col, blockSize;
 	int op;
+
+	int EventSet = PAPI_NULL;
+  	long long values[2];
+  	int ret;
+
+
+	ret = PAPI_library_init( PAPI_VER_CURRENT );
+	if ( ret != PAPI_VER_CURRENT )
+		std::cout << "FAIL" << endl;
+
+
+	ret = PAPI_create_eventset(&EventSet);
+		if (ret != PAPI_OK) cout << "ERROR: create eventset" << endl;
+
+
+	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
+	if (ret != PAPI_OK) cout << "ERROR: PAPI_L1_DCM" << endl;
+
+
+	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
+	if (ret != PAPI_OK) cout << "ERROR: PAPI_L2_DCM" << endl;
+
 	op=1;
 	do {
 		cout << endl << "1. Multiplication" << endl;
@@ -229,6 +273,12 @@ int main (int argc, char *argv[])
 		printf("Dimensions: lins=cols ? ");
    		cin >> lin;
    		col = lin;
+
+
+
+		// Start counting
+		ret = PAPI_start(EventSet);
+		if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
 
 		switch (op){
 			case 1:
@@ -245,8 +295,29 @@ int main (int argc, char *argv[])
 
 		}
 
+		ret = PAPI_stop(EventSet, values);
+  		if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
+  		printf("L1 DCM: %lld \n",values[0]);
+  		printf("L2 DCM: %lld \n",values[1]);
+
+		ret = PAPI_reset( EventSet );
+		if ( ret != PAPI_OK )
+			std::cout << "FAIL reset" << endl;
+
+
 	}while (op != 0);
 
+	ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
+	if ( ret != PAPI_OK )
+		std::cout << "FAIL remove event" << endl;
+
+	ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
+	if ( ret != PAPI_OK )
+		std::cout << "FAIL remove event" << endl;
+
+	ret = PAPI_destroy_eventset( &EventSet );
+	if ( ret != PAPI_OK )
+		std::cout << "FAIL destroy" << endl;
 
 
 }
